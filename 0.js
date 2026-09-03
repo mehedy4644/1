03afb940028a595f9106f2ada0acfaa47e4dd83a
mehedy4644 <mehedy4644@gmail.com>
2026-09-03 22:48:49 +0600

    Add files via upload

--- 0.js
+++ 0.js
@@ -113,8 +113,7 @@ object-fit:cover;
         AINCRAD BYPASS
       </p>
 
-      <input type="text" id="mehedy-key-input"
-      style="display:none;
+      <input type="text" id="mehedy-key-input" style="display:none;
       placeholder="ENTER KEY HERE" style="width:100%;padding:12px;margin-bottom:16px;
         border:1px solid rgba(0,255,204,0.4);border-radius:8px;
         background:rgba(7,11,25,0.6);color:#fff;text-align:center;
